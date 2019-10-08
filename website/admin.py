2dd0e1c20bc14175e2f32041bce610f15d2529ad
Rafael Santana <49356234+rafalmeida73@users.noreply.github.com>
2019-10-08 17:22:17 -0300

    site final

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Contato
+
+admin.site.register(Contato)
 
 # Register your models here.
--- website/views.py
+++ website/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
+from .models import Contato
 
 def home(request):
     return render(request, 'website/index.html')
 
 
 def contato(request):
-    return render(request, 'website/contato.html')
+    mensagem = ''
+    if request.method == 'POST':
+        try:
+            contato = {}
+            contato['email'] = request.POST.get('email')
+            contato['nome'] = request.POST.get('nome')
+            contato['sobrenome'] = request.POST.get('sobrenome')
+            contato['endereco'] = request.POST.get('endereco')
+            contato['mensagem'] = request.POST.get('mensagem')
+            contato['receber_noticias'] =  True if request.POST.get('receber_noticias') == 'on' else False 
+
+            Contato.objects.create(**contato)
+        except Exception as e:
+            mensagem = str(e)
+        else:
+            mensagem = 'Contato realizado com sucesso!'
+
+    return render(request, 'website/contato.html', {'mensagem': mensagem})
 
 
 def servicos(request):
