e664e1466888c9866923d52ac098e6104cc465ca
Rafael Santana <49356234+rafalmeida73@users.noreply.github.com>
2019-10-08 13:22:41 -0300

    index

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path, include
 from .views import (
     home,
+    contato,
+    servicos,
 )
 
 
 urlpatterns = [
     path('', home, name='website_home'),
+    path('contato', contato, name='website_contato'),
+    path('servicos', servicos, name='website_servicos'),
 
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'website/index.html')
+
+
+def contato(request):
+    return render(request, 'website/contato.html')
+
+
+def servicos(request):
+    return render(request, 'website/servicos.html')
