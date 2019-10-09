20972e769360248ac27651ddb9d1bb9d6f5f7042
Rafael Santana <49356234+rafalmeida73@users.noreply.github.com>
2019-10-09 17:41:16 -0300

    Finalizo

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from django.urls import include, re_path
 from .views import (
     home,
     lista_pessoas,
@@ -38,48 +39,49 @@ urlpatterns = [
     path('', home, name='core_home'),
     path('pessoas', lista_pessoas, name='core_lista_pessoas'),
     path('pessoa_novo', pessoa_novo, name='core_lista_pessoa_novo'),
-    path(r'pessoa-update/(?P<id>\d+)/$', pessoa_update,
+    re_path(r'pessoa-update/(?P<id>\d+)/$', pessoa_update,
          name='core_pessoa_update'),
-    path(r'pessoa-delete/(?P<id>\d+)/$', pessoa_delete,
+    re_path(r'pessoa-delete/(?P<id>\d+)/$', pessoa_delete,
          name='core_pessoa_delete'),
+
 
     path('veiculos', lista_veiculos, name='core_lista_veiculos'),
     path('veiculo_novo', veiculo_novo, name='core_lista_veiculo_novo'),
-    path(r'veiculo-update/(?P<id>\d+)/$', veiculo_update,
+    re_path(r'veiculo-update/(?P<id>\d+)/$', veiculo_update,
          name='core_veiculo_update'),
-    path(r'veiculo-delete/(?P<id>\d+)/$', veiculo_delete,
+    re_path(r'veiculo-delete/(?P<id>\d+)/$', veiculo_delete,
          name='core_veiculo_delete'),
-
-
 
 
     path('mov_rot', lista_movrotativos, name='core_lista_movrotativos'),
     path('mov_rot_novo', movrot_novo, name='core_lista_movrotativos_novo'),
-    path(r'mov-rot-update/(?P<id>\d+)/$', movrotativos_update, 
+    re_path(r'mov-rot-update/(?P<id>\d+)/$', movrotativos_update, 
         name='core_movrotativos_update'),
-    path(r'mov-rot-delete/(?P<id>\d+)/$', movrotativos_delete,
+    re_path(r'mov-rot-delete/(?P<id>\d+)/$', movrotativos_delete,
          name='core_movrotativos_delete'),
-
 
 
     path('mensalistas', lista_mensalistas, name='core_lista_mensalistas'),
     path('mensalistas_novo', mensalistas_novo, name='core_lista_mensalistas_novo'),
-    path(r'mensalista-update/(?P<id>\d+)/$', mensalista_update, name='core_mensalista_update'),
-    path(r'mensalista-delete/(?P<id>\d+)/$', mensalista_delete,
+    re_path(r'mensalista-update/(?P<id>\d+)/$', mensalista_update, name='core_mensalista_update'),
+    re_path(r'mensalista-delete/(?P<id>\d+)/$', mensalista_delete,
          name='core_mensalista_delete'),
+
 
     path('mov_mensalista', lista_movmensalistas, name='core_lista_movmensalistas'),
     path('mov_mensal_novo', movmensal_novo, name='core_lista_movmensalista_novo'),
-    path(r'mov-mensalista-update/(?P<id>\d+)/$', movmensalista_update, 
+    re_path(r'mov-mensalista-update/(?P<id>\d+)/$', movmensalista_update, 
         name='core_movmensalista_update'),
-    path(r'mov-mensalista-delete/(?P<id>\d+)/$', movmensalista_delete,
+    re_path(r'mov-mensalista-delete/(?P<id>\d+)/$', movmensalista_delete,
          name='core_movmensalista_delete'),
+
 
      path('relatorio', Pdf.as_view(), name='relatorio_pdf'),
      path('relatorio_mensalista', PdfMensalista.as_view(), name='relatorioMensalista_pdf'),
      path('relatorio_movmensalista', PdfMovMensalista.as_view(), name='relatorioMovMensalista_pdf'),
      path('relatorio_movrotativo', PdfMovRotativo.as_view(), name='relatorioMovRotativo_pdf'),
      path('relatorio_pessoas', PdfPessoas.as_view(), name='relatorioPessoas_pdf'),
+
 
      path('relatorio-csv', ExportarParaCSV.as_view(), name='relatorio_csv'),
      path('relatoriomensalista-csv', CSVMensalista.as_view(), name='relatorioMensalista_csv'),
